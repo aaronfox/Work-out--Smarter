bae64d51902806096171c4241368f1badfae6403
aaronfox <aaron.m.fox3@gmail.com>
2020-11-21 19:17:14 -0500

    added app icons

---
+++ WorkoutSmarter/app/src/main/java/edu/cse570afox/workoutsmarter/WorkoutHistory.java
@@ -0,0 +1,14 @@
+package edu.cse570afox.workoutsmarter;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class WorkoutHistory extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_workout_history);
+    }
+}
